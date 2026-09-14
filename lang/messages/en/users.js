82f8282f1c13a5006bc4cc17ba1be4a07458775c
Andrei Chepakovich <andreichepakovich@gmail.com>
2026-09-13 20:54:44 -0700

    first commit

---
+++ lang/messages/en/users.js
@@ -0,0 +1,7 @@
+export const MESSAGES = {
+    inputLabel: "How many buttons to create?",
+    goButton: "Go",
+    invalidInput: "Please enter a number from 3 to 7.",
+    excellent: "Excellent memory!",
+    wrongOrder: "Wrong order!"
+};
